82f71377f3ebfe214cf40b9a51428178353af334
0ppilif <filippomoreno.128@gmail.com>
2025-08-24 22:49:02 -0400

    aaaa

--- monitoreo/dispositivos/views.py
+++ monitoreo/dispositivos/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def inicio(request):
+    dispositivos = [
+        {"nombre": "Medidor Electrico", "consumo": 30},
+        {"nombre": "Refrigeradores", "consumo": 45},
+        {"nombre": "Aire Acondicionado", "consumo": 300},
+        {"nombre": "Estufa", "consumo": 150}
+    ]
+    
+    consumo_maximo = 100
+
+    return render(request, "dispositivos/inicio.html", {
+        "dispositivos": dispositivos,
+        "consumo_maximo": consumo_maximo
+        })
