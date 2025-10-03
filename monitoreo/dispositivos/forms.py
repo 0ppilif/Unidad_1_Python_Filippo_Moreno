465e5f34ad612e272cfd6cbf1eb71f69616c866a
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2025-10-03 13:37:27 -0300

    aaaaaaaaaaaaaaaaaa

--- monitoreo/dispositivos/forms.py
+++ monitoreo/dispositivos/forms.py
@@ -3,11 +3,13 @@ from .models import dispositivo
 
 class DispositivoForm(forms.ModelForm):
     class Meta:
-        model = dispositivo
+        model = Dispositivo
         fields = ['nombre', 'categoria', 'zona', 'consumo_maximo', 'estado']
         widgets == {
             'nombre': forms.TextInput(attrs={'class': ' form-control'}),
             'consumo_maximo': forms.NumberInput(attrs={'class': 'form-control'}),
             'categoria': forms.Select(attrs={'class': 'form-select'}), #butstrappppppp
-            
+            "zona": forms.Select(attrs={'class': 'form-select'}),
+            "estado": forms.Select(attrs={'class': 'form-select'}),
+            'imagen':forms.FileInput(attrs={'class': 'form-control'}),
         }
--- monitoreo/monitoreo/urls.py
+++ monitoreo/monitoreo/urls.py
@@ -16,10 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
-from dispositivos.views import inicio
+from dispositivos.views import inicio, dispositivo, crear_dispositivo, editar_dispositivo, eliminar_dispositivo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', inicio)#se muestra en la raiz
+    path('', inicio, name= 'inicio'),
+    path('dispositivos/', inicio, name="listar_dispositivos"),
+    path('dispositivos/<int:dispositivo_id>/', dispositivo, name="dispositivo"),
+    path('dispositivos/crear/', crear_dispositivo, name="crear_dispositivo"),
+    path('dispositivos/eliminar/<int:dispositivo_id>/', eliminar_dispositivo, name="eliminar_dispositivo"),
+    path('dispositivos/editar/<int:dispositivo_id>/', editar_dispositivo, name="editar_dispositivo"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
